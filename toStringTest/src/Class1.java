f944add99016f6f5074602df9e868f4ab413a367
freehoon <freehoon009@gmail.com>
2019-04-09 13:16:12 +0900

    2019.04.09

---
+++ toStringTest/src/Class1.java
@@ -0,0 +1,16 @@
+public class Class1 {
+
+    private String str1 = "str1";
+    private String str2 = "str2";
+
+    public Class1() {
+    }
+
+    @Override
+    public String toString() {
+        return "Class1{" +
+                "str1='" + str1 + '\'' +
+                ", str2='" + str2 + '\'' +
+                '}';
+    }
+}
---
+++ toStringTest/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+
+        System.out.println(class1);
+
+    }
+}
